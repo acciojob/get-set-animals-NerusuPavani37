8d30d0f53ea9eb40ac7532d35da410a87567d0a1
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-11 20:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Animal {}
+//complete this code
+class Animal {
+	constructor(species){
+	    this.species = species;
+	}
+	
+	get speciesName(){
+	    return this.species;
+	}
+  makeSound(){
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+  bark(){
+    console.log('woof')
+  }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+  purr(){
+    console.log('purr');
+  }
+}
+
+let cat = new Cat("Siamese");
+cat.makeSound();
+cat.purr();
+console.log(cat.speciesName);
+let dog = new Dog("Golden Retriever");
+dog.makeSound();
+console.log(dog.speciesName);
+dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
